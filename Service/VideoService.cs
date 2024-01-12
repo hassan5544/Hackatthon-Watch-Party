9b97d999b7c1642255c1e99fdaf379078eb104a5
Hassan-Abdulbaqi <88453351+Hassan-Abdulbaqi@users.noreply.github.com>
2024-01-12 18:02:04 +0300

    Added Video Service

---
+++ Service/VideoService.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class VideoService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public VideoService(IRepositoryManager repository, ILoggerManager
+        logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
