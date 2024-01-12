62a0324f46df2b1bd99c7fc5698de8686ffac8ed
hassan5544 <hmh.lol55@gmail.com>
2024-01-12 16:28:12 +0300

    commit by hassan add Seed data of user

---
+++ Repository/Configuartion/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuartion
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(
+                new User
+                {
+                    id = new Guid(), 
+                    Name = "test",
+                    PassWord = "test",
+                    Email = "test",
+                }
+                );
+            ;
+        }
+    }
+}
