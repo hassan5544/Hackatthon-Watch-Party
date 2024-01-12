b7f1593d412b633d29ed7055b288296fb8aadf79
hassan5544 <hmh.lol55@gmail.com>
2024-01-12 17:57:11 +0300

    Add ServiceManager And Pipline

--- Hackatthon-Watch-Party/Program.cs
+++ Hackatthon-Watch-Party/Program.cs
@@ -9,7 +9,7 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
-
+builder.Services.ConfigureSqlContext(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
---
+++ Service/ServiceManager.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IUserService> _userService;
+        private readonly Lazy<IVideoService> _videoService;
+
+        public ServiceManager(IRepositoryManager repositoryManager , ILoggerManager loggerManager)
+        {
+            _userService = new Lazy<IUserService>(() => new UserService(repositoryManager , loggerManager));
+        }
+        public IUserService UserService => _userService.Value;
+
+        public IVideoService VideoService => throw new NotImplementedException();
+    }
+}
